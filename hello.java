fd12747ec3ded6b3e032f0f5c7b02889bfd3e4ac
G-crafts <katelynn.graham@malad.us>
2025-04-30 19:40:05 -0600

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,19 @@
+// This is my Hello World class
+//Katelynn Graham
+//April 30, 2025
+//Intro To Informations Technology 
+//Hello World with my information
+
+
+
+public class hello {
+    //The string for my printed out hello world
+    public static void main(String[] args){
+        //This will print out the word "Hello"
+        System.out.println("Hello!");
+        //This is going to print out my full name
+        System.out.println("My name is Katelynn Graham");
+        //This is the school that I am attending
+        System.out.println("I go to Braintree Academy");
+    }
+}
